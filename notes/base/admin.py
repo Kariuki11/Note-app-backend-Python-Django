9f33c8d72c00ae587cef7578801d25be277a50b4
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-14 10:06:53 +0300

    "Updated admin.py to add custom NoteAdmin class with list_display, search_fields, and list_filter"

--- notes/base/admin.py
+++ notes/base/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Note
 # Register your models here.
 
-admin.site.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ["title", "category", "created", "updated"]
+    search_fields = ["title", "category"]
+    list_filter = ["category", "created", "updated"]
+
+admin.site.register(Note, NoteAdmin)
