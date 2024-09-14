480397b7137405ce17542ad94281c3de33d5c40d
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-14 09:40:58 +0300

    "Added POST method handling to notes view, including serialization and error response"

--- notes/base/views.py
+++ notes/base/views.py
@@ -3,6 +3,7 @@ from notes.base.models import Note
 from notes.base.serializers import NoteSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 # Create your views here.
 @api_view(["GET", "POST"])
@@ -11,3 +12,10 @@ def notes(request):
         notes = Note.objects.all()
         serializer = NoteSerializer(notes, many=True)
         return Response(serializer.data)
+    
+    elif request.method =="POST":
+        serializer = NoteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
