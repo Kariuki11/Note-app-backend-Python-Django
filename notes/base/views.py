603b732be225c3b3214a330ffcbcf599201bc024
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-14 09:28:06 +0300

    "Added REST framework imports and decorators to notes view, and returned Response object with serialized data."

--- notes/base/views.py
+++ notes/base/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from notes.base.models import Note
 from notes.base.serializers import NoteSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
-
+@api_view
 def notes(request):
     notes = Note.objects.all()
     serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
