15eb6b84561130acdaf72455fa7b46269e66003a
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-14 09:24:18 +0300

    "Added imports and implemented notes view to serialize all notes"

--- notes/base/views.py
+++ notes/base/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from notes.base.models import Note
+from notes.base.serializers import NoteSerializer
 
 # Create your views here.
 
 def notes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
