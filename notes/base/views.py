4bcf94aabc65917c48b1b1c38d1bcfb69548d937
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-16 10:40:43 +0300

    changes

--- notes/base/views.py
+++ notes/base/views.py
@@ -29,6 +29,13 @@ def note_detail(request, slug):
     except Note.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
     
-    If request.method == "GET":
-    serializer = NoteSerializer(note)
-    return Response(serializer.data)
+    if request.method == "GET":
+        serializer = NoteSerializer(note)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = NoteSerializer(note, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
