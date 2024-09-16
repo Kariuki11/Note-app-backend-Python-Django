c6281b50f94edc8a30cad3ec4be6eecc01821048
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-16 11:19:51 +0300

    CHANGES

--- notes/base/urls.py
+++ notes/base/urls.py
@@ -3,6 +3,7 @@ from .import views
 
 urlpatterns = [
     path('notes/', views.notes, name='notes'),
-    #path("notes/<slug:slug", views.note_detail, name="note-detail"),
     path('notes/<slug:slug>/', views.note_detail, name='note-detail'),
 ]
+
+#
