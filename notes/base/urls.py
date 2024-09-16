dd54391512e58787b13da42b1afd8f93b69fc360
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-16 11:22:47 +0300

    New changes

--- notes/base/urls.py
+++ notes/base/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('notes/<slug:slug>/', views.note_detail, name='note-detail'),
 ]
 
-#
+# ENDPOINTS.
+# GET_ALL_NOTES_and_CREATE_NEW_NOTE = "http://127.0.0.1:8004/notes/"
+# GET_SPECIDIED_NOTE = "http://127.0.0.1:8004/notes/note-slug"
