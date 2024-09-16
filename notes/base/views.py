40110de165d99b7005ee007e45d7360ab08fe5e3
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-16 10:54:58 +0300

    New changes

--- notes/base/views.py
+++ notes/base/views.py
@@ -39,3 +39,7 @@ def note_detail(request, slug):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        note.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
