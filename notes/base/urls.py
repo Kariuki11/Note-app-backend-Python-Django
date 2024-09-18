3df19b781377eaac0248b62151a6ad334197de44
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-19 01:54:49 +0300

    changes

--- notes/base/urls.py
+++ notes/base/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
 ]
 
 # ENDPOINTS.
-# GET_ALL_NOTES_and_CREATE_NEW_NOTE = "http://127.0.0.1:8004/notes/"
-# GET_SPECIDIED_NOTE = "http://127.0.0.1:8004/notes/note-slug"
+# GET_ALL_NOTES_and_CREATE_NEW_NOTE = "127.0.0.1:8008/notes/"
+# GET_SPECIDIED_NOTE = "http://127.0.0.1:8008/notes/note-slug"
