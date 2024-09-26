a6ebf4df07927813d059312d6fa0d1710acfaaef
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-26 21:53:58 +0300

    changes

--- notes/base/views.py
+++ notes/base/views.py
@@ -3,6 +3,7 @@ from .models import Note
 from .serializers import NoteSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.db.models import Q
 from rest_framework import status
 
 # Create your views here.
